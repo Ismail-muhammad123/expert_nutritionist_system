917e873266b3cc5fe8265df51f990b1af745da5c
Ismail Muhammad <ismaeelmuhammad123@gmail.com>
2022-07-11 00:11:35 +0100

    settings

--- nutrition_expert/settings.py
+++ nutrition_expert/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k3^o9tgr&+(zsbz#d9i9onojz=3rdhny--jh8@d@&^ewskotbj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
